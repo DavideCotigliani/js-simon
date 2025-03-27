f869dcc0fd3b3fec5305bfb733d9a055fef124c1
DavideCotigliani <davide.cotigliani@gmail.com>
2025-03-27 14:57:01 +0100

    [UPD] Created the Mathrandom for numberslist

--- simon_says_dom/js/script.js
+++ simon_says_dom/js/script.js
@@ -1,8 +1,16 @@
 // definisco le variabili e recupero gli elementi per iniziare il countdown
 const countdown = document.getElementById('countdown');
-let seconds = 5;
-
+let seconds = 30;
 countdown.innerText= seconds--
+
+// variabili per i numeri casuali e recupero degli elementi
+const numberslist = document.getElementById('numbers-list')
+
+function randomNumbers (){
+    numberslist.innerText= Math.floor(Math.random() * 5) +1 ;
+}
+randomNumbers()
+
 
 //strutturo il corpo del programma del countdown
 const interval = setInterval(function(){
