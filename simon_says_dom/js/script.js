ccde448f847696a20bc08f38b9af415b585865fd
DavideCotigliani <davide.cotigliani@gmail.com>
2025-03-27 14:26:46 +0100

    [UPD] Created the variables and recovered the elements, create the countdown structure

--- simon_says_dom/js/script.js
+++ simon_says_dom/js/script.js
@@ -1,3 +1,17 @@
 // definisco le variabili e recupero gli elementi per iniziare il countdown
 const countdown = document.getElementById('countdown');
-let seconds = 30;
+let seconds = 5;
+
+countdown.innerText= seconds--
+
+//strutturo il corpo del programma del countdown
+const interval = setInterval(function(){
+if(seconds != -1){
+countdown.innerText= seconds
+}
+else{
+    clearInterval(interval)
+}
+//per decrementare i secondi
+seconds--
+},1000)
