eb5ebb54ae3c57fdba5335830069ff1263e75205
DavideCotigliani <davide.cotigliani@gmail.com>
2025-03-27 16:35:54 +0100

    [UPD] Adding the addEvenListener to generate the result

--- simon_says_dom/js/script.js
+++ simon_says_dom/js/script.js
@@ -1,9 +1,11 @@
 // definisco le variabili e recupero gli elementi per iniziare il countdown
 const countdown = document.getElementById('countdown');
-let seconds = 30;
+let seconds = 5;
 countdown.innerText= seconds--
 const instructions = document.getElementById ('instructions')
 const form = document.getElementById('answers-form')
+const button = document.querySelector('.btn btn-primary mt-2 d-block mx-auto')
+const number = document.querySelector('.form-control')
 
 // variabili per i numeri casuali e recupero degli elementi
 const numberslist = document.getElementById('numbers-list')
@@ -17,7 +19,6 @@ function randomNumbers (){
     numberslist.innerHTML=numbers.join(' ')
 }
 randomNumbers()
-
 
 //strutturo il corpo del programma del countdown
 const interval = setInterval(function(){
@@ -34,7 +35,14 @@ else{
 seconds--
 },1000)
 
+// far apparire dopo 30 secondi i campi
 const appear = setTimeout(function(){
 form.classList.remove("d-none")
 
-},30000)
+},5000)
+
+button.addEventListener ('click',function(){
+let correctNumbers = [];
+let correctCount = 0;
+
+})
